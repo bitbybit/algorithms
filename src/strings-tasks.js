023f39dee4e277ed4f8625d98a29bc8bc13b81fd
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-12-14 15:41:02 +0100

    feat: reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -396,8 +396,16 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const words = str.split(' ');
+
+  const result = [];
+
+  words.forEach((word) => {
+    result.push(word.split('').reverse().join(''));
+  });
+
+  return result.join(' ');
 }
 
 /**
