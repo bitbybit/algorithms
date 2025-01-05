808d9140d10194499ed7982f358d29cd76a8c973
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-05 16:10:32 +0100

    feat: Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,23 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create a repeating string based on the given parameters
- *  
- * @param {String} str string to repeat
- * @param {Object} options options object 
- * @return {String} repeating string
- * 
+ *
+ * @param {unknown} str string to repeat
+ * @param {Object} options options object
+ * @param {number|undefined} options.repeatTimes
+ * @param {string|undefined} options.separator
+ * @param {unknown|undefined} options.addition
+ * @param {number|undefined} options.additionRepeatTimes
+ * @param {string|undefined} options.additionSeparator
+ * @return {string} repeating string
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {
+  repeatTimes = 1,
+  separator = '+',
+  addition,
+  additionRepeatTimes = 1,
+  additionSeparator = '|'
+} = {
+  repeatTimes: 1,
+  separator: '+',
+  addition: undefined,
+  additionRepeatTimes: 1,
+  additionSeparator: '|'
+}) {
+  const strString = String(str);
+  const additionString = String(addition);
+
+  const hasAddition = addition !== undefined;
+
+  const result = [];
+  const additionResult = [];
+
+  for (let j = 0; j < additionRepeatTimes; j++) {
+    additionResult.push(additionString);
+  }
+
+  for (let i = 0; i < repeatTimes; i++) {
+    if (!hasAddition) {
+      result.push(strString);
+
+      continue;
+    }
+
+    result.push(
+      `${strString}${additionResult.join(additionSeparator)}`
+    );
+  }
+
+  return result.join(separator);
 }
 
 module.exports = {
