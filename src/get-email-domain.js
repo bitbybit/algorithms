b64bb69a522e8345e1afc638c9efbe3f1ce1dbcf
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-06 18:36:43 +0100

    feat: Get email domain

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,18 +1,15 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given an email address, return it's domain.
  *
- * @param {String} email
- * @return {String}
+ * @param {string} email
+ * @return {string}
  *
  * @example
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  return email.split('@').at(-1);
 }
 
 module.exports = {
