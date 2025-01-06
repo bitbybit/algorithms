98d42774482bb5b2f8174edf73a72be69b77972d
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-06 17:17:21 +0100

    feat: Encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,18 +1,33 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given a string, return its encoding version.
  *
- * @param {String} str
- * @return {String}
+ * @param {string} str
+ * @return {string}
  *
  * @example
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const result = [];
+
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count += 1;
+
+      continue;
+    }
+
+    result.push(
+      `${count > 1 ? count : ''}${str[i]}`
+    );
+
+    count = 1;
+  }
+
+  return result.join('');
 }
 
 module.exports = {
