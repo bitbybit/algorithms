6a1ecd05e2f4206469a4cbfd417a7b0bbb1c32fe
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-06 19:18:44 +0100

    feat: Minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,13 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
  * in the neighboring cells. Starting off with some arrangement of mines
  * we want to create a Minesweeper game setup.
  *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
+ * @param {Array<Array<boolean>>} matrix
+ * @return {Array<Array<number>>}
  *
  * @example
  * matrix = [
@@ -23,9 +21,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+
+  const result = Array(rows).fill([])
+    .map(() => Array(columns).fill(0));
+
+  for (let row = 0; row < rows; row++) {
+    for (let column = 0; column < columns; column++) {
+      const isTopLeft = matrix?.[row - 1]?.[column - 1] ?? false;
+      const isTop = matrix?.[row - 1]?.[column] ?? false;
+      const isTopRight = matrix?.[row - 1]?.[column + 1] ?? false;
+      const isLeft = matrix[row]?.[column - 1] ?? false;
+      const isRight = matrix[row]?.[column + 1] ?? false;
+      const isBottomLeft = matrix?.[row + 1]?.[column - 1] ?? false;
+      const isBottom = matrix?.[row + 1]?.[column] ?? false;
+      const isBottomRight = matrix?.[row + 1]?.[column + 1] ?? false;
+
+      result[row][column] =
+        (isTopLeft ? 1 : 0) +
+        (isTop ? 1 : 0) +
+        (isTopRight ? 1 : 0) +
+        (isLeft ? 1 : 0) +
+        (isRight ? 1 : 0) +
+        (isBottomLeft ? 1 : 0) +
+        (isBottom ? 1 : 0) +
+        (isBottomRight ? 1 : 0);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
