ca1681064294ec8ac993ca6c72ed9d597b285eb3
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-06 14:50:56 +0100

    feat: Common character count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,19 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given two strings, find the number of common characters between them.
  *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
  *
  * @example
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const [sMax, sMin] = s1.length > s2 ?
+    [s1.split(''), s2.split('')] :
+    [s2.split(''), s1.split('')];
+
+  let result = 0;
+
+  for (const char of sMin) {
+    const sMaxIndex = sMax.indexOf(char);
+
+    if (sMaxIndex !== -1) {
+      result += 1;
+
+      sMax.splice(sMaxIndex, 1);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
