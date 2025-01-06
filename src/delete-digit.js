994c1bd3b6cd00e64681c61cd40d40918b4dce3f
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-06 15:27:12 +0100

    feat: Delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,19 +1,29 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
  *
- * @param {Number} n
- * @return {Number}
+ * @param {number} n
+ * @return {number}
  *
  * @example
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const digits = String(n).split('');
+
+  let result = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    const digitsCropped = [...digits.slice(0, i), ...digits.slice(i + 1)];
+    const nCropped = Number(digitsCropped.join(''));
+
+    if (nCropped > result) {
+      result = nCropped;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
