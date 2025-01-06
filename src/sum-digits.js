4110e11e8e614eb7a26540d91546cd21a5ab1488
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-06 19:38:21 +0100

    feat: Sum digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,20 +1,28 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given a number, replace this number with
  * the sum of its digits until we get to a one digit number.
  *
- * @param {Number} n
- * @return {Number}
+ * @param {number} n
+ * @return {number}
  *
  * @example
  * For 100, the result should be 1 (1 + 0 + 0 = 1)
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let result = n;
+
+  while (result > 9) {
+    const nList = String(result).split('').map(Number);
+
+    result = nList.reduce(
+      (accumulator, currentValue) =>
+        accumulator + currentValue,
+      0);
+  }
+
+  return result;
 }
 
 module.exports = {
