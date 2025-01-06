3eb85ea2df25f4938cc758a4152330152e982b5d
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-06 19:30:03 +0100

    feat: Sort by height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,19 +1,33 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given an array with heights, sort them except if the value is -1.
  *
- * @param {Array} arr
- * @return {Array}
+ * @param {Array<number>} arr
+ * @return {Array<number>}
  *
  * @example
  * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const filtered = arr.filter((height) => height !== -1);
+  const sorted = filtered.sort((a, b) => a - b);
+
+  let restoredIndex = 0;
+
+  return arr.map((height) => {
+    const isFiltered = height === -1;
+
+    if (isFiltered) {
+      return height;
+    } else {
+      const result = sorted[restoredIndex];
+
+      restoredIndex += 1;
+
+      return result;
+    }
+  });
 }
 
 module.exports = {
