92c1e397cdc44110d5fe23056bed9a1fca40b02d
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-07 15:43:01 +0100

    feat: createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -346,8 +346,14 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((result, _, i) => {
+    if (i % chunkSize === 0) {
+      result.push(arr.slice(i, i + chunkSize));
+    }
+
+    return result;
+  }, []);
 }
 
 /**
