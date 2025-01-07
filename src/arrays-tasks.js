3e6ff829138bc0c8ff894d18c8f05260910cd0bd
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-07 16:08:19 +0100

    feat: getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -424,8 +424,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_row, rowIndex) =>
+    Array.from({ length: n }, (_column, columnIndex) =>
+      rowIndex === columnIndex ? 1 : 0
+    )
+  );
 }
 
 /**
