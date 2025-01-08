c8f209b1434272d7e7d79fa5eef1481dbc13e55c
bitbybit <1628191+bitbybit@users.noreply.github.com>
2025-01-08 15:52:36 +0100

    feat: isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -235,8 +235,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
 }
 
 /**
